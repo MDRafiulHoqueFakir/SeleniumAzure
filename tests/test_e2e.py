28abbf3d6942853c4604839f0bf152bae3fbd6a0
MDRafiulHoqueFakir <md.rafiul.aiub@gmail.com>
2025-11-24 17:49:59 +0600

    Fix ModuleNotFoundError by adding sys.path configuration to test files

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 import allure
 from pages.login_page import LoginPage
--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 import allure
 from selenium.webdriver.common.by import By
